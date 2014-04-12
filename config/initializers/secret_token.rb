5efed270e1b12f854bc31556310e7d21e3496a67
Pablo Vergel & Tony Ta <pair+pvergel.tonyta@devbootcamp.com>
2014-04-12 12:56:58 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PostGISTryout::Application.config.secret_key_base = '841666096bc823a81de1ce0b1a7c15452a073944118dc2264fd4380505ec5d87273a5e0461f009d0b64111062927372564c0dbe2ab0db5423b34597b9903d456'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PostGISTryout::Application.config.session_store :cookie_store, key: '_postGIS_tryout_session'
